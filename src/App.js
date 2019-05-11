309607af38607a4822ea8eaad458bc5186c81cad
CesarRivasP <cesarjose58@gmail.com>
2019-05-10 22:26:16 -0400

    IMplemented routes of components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Home from './pages/home'
+import Recipe from './pages/recipe'
+// import Timer from './pages/timer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <header>
+          <Link to="/">Recetas</Link>
+        </header>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/recipe/:recipeID" component={Recipe} />
+          {/* <Route path="/timer" component={Timer} /> */}
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
