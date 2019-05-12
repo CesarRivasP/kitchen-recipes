2b3c5d40093f60a2c721e0f5c247ccc332285318
CesarRivasP <cesarjose58@gmail.com>
2019-05-12 14:30:10 -0400

    IMplemented createBrowserHistory and ReactGA components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { Router, Route, Link } from "react-router-dom";
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history'
 import Home from './pages/home'
 import Recipe from './pages/recipe'
-// import Timer from './pages/timer'
+import Timer from './pages/timer'
 import './App.css';
+
+// Creacion del plugin para la historia del browser
+const history = createBrowserHistory(); //esto nos permite trackear que paginas visitan los usuarios
+
+//inicializar ReactGA
+ReactGA.initialize('UA-000000-01'); //id fake
+// Trackear la pageview inicial cuando entramos en la aplicacion
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+// hay que agregar este evento para que indique cuando un usuario cambia de pagina. COn esto se puede trackear una page view con react
+history.listen(function(location){
+  ReactGA.pageview(window.location.pathname + window.location.search) //url completa que esta visitando el usuario
+  // pathname es la url en si
+  // search son todos los querys que tenemos dentro de la url
+})
 
 class App extends Component {
   render(){
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <header>
           <Link to="/">Recetas</Link>
         </header>
@@ -16,9 +33,9 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Home} />
           <Route path="/recipe/:recipeID" component={Recipe} />
-          {/* <Route path="/timer" component={Timer} /> */}
+          <Route path="/timer" component={Timer} />
         </main>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
