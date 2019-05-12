8e80708aaae45ce1339f88452b2d4cfaf8ded7c3
CesarRivasP <cesarjose58@gmail.com>
2019-05-12 15:01:51 -0400

    Implemented methods for web share api

--- src/pages/recipe.js
+++ src/pages/recipe.js
@@ -29,6 +29,26 @@ class Recipe extends Component {
     })
   }
 
+  share = (e) => {
+    e.preventDefault(); //cancelar el evento de click
+    // PAra detectar si el browser tiene la 'web share api'
+    if(!navigator.share){
+      alert('Tu navegador no soporta la web share api');
+      return;
+    }
+
+    // Traer la receta en el estado de la app
+    const { recipe } = this.state;
+    // llamar a web share api
+    navigator.share({ //esto devuelve una promesa
+      title: recipe.name,
+      text: 'Receta de platzi', //descriptcion del link donde se comenta de que es el contenido
+      url: document.location.href
+    })
+      .then(() => alert('Contenido compartido'))
+      .catch((error) => alert('Hubo un error'))
+  }
+
   render(){
     const { recipe, isLoading } = this.state;
 
@@ -50,6 +70,9 @@ class Recipe extends Component {
           <div className="info">
             <h1>{recipe.name}</h1>
             <p>{ recipe.origin }</p>
+          </div>
+          <div>
+            <a onClick={this.share}>Compartir</a>
           </div>
         </div>
 
